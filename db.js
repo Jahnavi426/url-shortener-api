fe6904e52433caa62d5d657c5b1edd45002af214
Jahnavims <jahnavims123@gmail.com>
2026-03-06 22:52:42 +0530

    Initial commit - URL Shortener API

---
+++ db.js
@@ -0,0 +1,29 @@
+const { MongoClient } = require("mongodb");
+
+// MongoDB connection string
+const uri = "mongodb+srv://jahnavims:2112%40Admin@cluster0.ujq0yeu.mongodb.net/?appName=Cluster0";
+//const uri = "mongodb+srv://jahnavims:2112%40Admin@cluster0.ujq0yeu.mongodb.net/ProjectDB?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
+
+let db;
+
+async function connectDB() {
+  try {
+    await client.connect();
+    console.log("MongoDB Connected");
+
+    db = client.db("ProjectDB");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+}
+
+function getDB() {
+  return db;
+}
+
+module.exports = { connectDB, getDB };
+
+
---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const shortid = require("shortid");
+const { MongoClient } = require("mongodb");
+
+const app = express();
+app.use(express.json());
+
+// MongoDB connection string
+const uri = "mongodb+srv://jahnavims:2112%40Admin@cluster0.ujq0yeu.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri);
+
+async function startServer() {
+  try {
+    // Connect to MongoDB
+    await client.connect();
+    console.log("MongoDB Connected");
+
+    const db = client.db("ProjectDB");
+    const urlsCollection = db.collection("urls");
+
+    // Home route
+    app.get("/", (req, res) => {
+      res.send("URL Shortener API Running");
+    });
+
+    // Create short URL
+    app.post("/shorten", async (req, res) => {
+      const { url } = req.body;
+
+      if (!url) {
+        return res.status(400).json({ error: "URL is required" });
+      }
+
+      const shortCode = shortid.generate();
+
+      await urlsCollection.insertOne({
+        shortCode: shortCode,
+        originalUrl: url
+      });
+
+      res.json({
+        shortUrl: `http://localhost:5000/${shortCode}`
+      });
+    });
+
+    // Redirect to original URL
+    app.get("/:code", async (req, res) => {
+      const result = await urlsCollection.findOne({
+        shortCode: req.params.code
+      });
+
+      if (result) {
+        res.redirect(result.originalUrl);
+      } else {
+        res.status(404).send("URL not found");
+      }
+    });
+
+    // Start server
+    app.listen(5000, () => {
+      console.log("Server running on port 5000");
+    });
+
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
+}
+
+startServer();
